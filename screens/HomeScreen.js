fd566315cf140463a3291b8aae0e73076d9436f0
Hannah Dadd <hannahbdadd@googlemail.com>
2017-11-22 00:04:54 +0000

    Welcome screen in stack navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
       title: 'Welcome',
