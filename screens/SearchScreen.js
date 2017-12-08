260dc33219cee5f386c70f735530689cf5c90338
Hannah Dadd <hannahbdadd@googlemail.com>
2017-12-08 11:09:38 +0000

    Search for authors or posts

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TextInput, View, FlatList, StyleSheet, Button } from 'react-native';
+import { Text, TextInput, View, StyleSheet, Button, Picker } from 'react-native';
 import CheckBox from 'react-native-checkbox';
 import ArticleDisplay from '../components/ArticleDisplay';
 
@@ -11,12 +11,13 @@ export default class SearchScreen extends React.Component {
     constructor(props) {
       super(props);
       this.state = {searchFor: '',
-                    articles: []};
+                    articles: [],
+                    searchIn: 'posts'};
     }
 
     // Query rest api for data- use fetch api
     getJSONData() {
-      fetch('https://www.theedgesusu.co.uk/wp-json/wp/v2/posts' + '?search=' + 'hannah', {
+      fetch('https://www.theedgesusu.co.uk/wp-json/wp/v2/' + this.state.searchIn + '?search=' + this.state.searchFor, {
         method: 'GET',
         headers: {
           Accept: 'application/json',
@@ -33,13 +34,22 @@ export default class SearchScreen extends React.Component {
 
     render() {
       const { navigate } = this.props.navigation;
+      let results;
 
       // The view where the articles are displayed
-      let results = this.state.articles.map((article) => {
-        return <View key={article.id} style={styles.item}>
-                <ArticleDisplay title={article.title.rendered}></ArticleDisplay>
-              </View>
-      })
+      if(this.state.searchIn === 'posts'){
+        results = this.state.articles.map((article) => {
+          return <View key={article.id} style={styles.item}>
+                  <ArticleDisplay title={article.title.rendered}></ArticleDisplay>
+                </View>
+        })
+      } else if(this.state.searchIn === 'users'){
+        results = this.state.articles.map((article) => {
+          return <View key={article.id} style={styles.item}>
+                  <ArticleDisplay title={article.name}></ArticleDisplay>
+                </View>
+        })
+      }
       return (
         <View style={{flex: 1, flexDirection: 'column'}}>
             <View style={{padding: 10}}>
@@ -48,6 +58,12 @@ export default class SearchScreen extends React.Component {
                   placeholder="Type here to search!"
                   onChangeText={(text) => this.setState({searchFor:text})}
               />
+              <Picker
+                selectedValue={this.state.searchIn}
+                onValueChange={(itemValue) => this.setState({searchIn: itemValue})}>
+                <Picker.Item label="Post" value="posts" />
+                <Picker.Item label="Author" value="users" />
+              </Picker>
               <Button
                   onPress={this.getJSONData.bind(this)}
                   title="Search"/>
@@ -65,7 +81,6 @@ export default class SearchScreen extends React.Component {
     },
     item: {
       padding: 10,
-      fontSize: 18,
       height: 44,
     },
   })
