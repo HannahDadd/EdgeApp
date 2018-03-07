4f9108082cbab17433c9417289a1405c36d3b8fc
Hannah Dadd <hannahbdadd@googlemail.com>
2018-03-07 12:02:50 +0000

    Buttons don't press on load for culture articles

--- screens/sections/CultureScreen.js
+++ screens/sections/CultureScreen.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { Button, View } from 'react-native';
 
 export default class CultureScreen extends React.Component {
-
-  // Load the culture sub section screen
-  onScreenChange(sectionTitle, section) {
-    this.props.navigation.navigate('Section', {sectionTitle: sectionTitle, section: section, navigate: this.props.navigation});
-  };
+  constructor(props) {
+    super(props);
+    this.state = { 
+        navigate: this.props.navigation.navigate
+    };
+  }
   
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column', padding: 10}}>
         <Button title="All Culture Articles" 
-          onPress={this.onScreenChange("All Culture Articles", "culture")}/>
-        <Button title="Theatre" onPress={this.onScreenChange("Theatre", "theatre")}/>
-        <Button title="Television" onPress={this.onScreenChange("Television", "television")}/>
-        <Button title="Video Games" onPress={this.onScreenChange("Video Games", "video-games")}/>
-        <Button title="Literature" onPress={this.onScreenChange("Literature", "literature")}/>
+          onPress={() => this.state.navigate('Section', {sectionTitle: "All Culture Articles", section: "culture", navigate: this.state.navigate})}/>
+        <Button title="Theatre"
+          onPress={() => this.state.navigate('Section', {sectionTitle: "Theatre Articles", section: "theatre", navigate: this.state.navigate})}/>
+        <Button title="Television"
+          onPress={() => this.state.navigate('Section', {sectionTitle: "Television Articles", section: "television", navigate: this.state.navigate})}/>
+        <Button title="Video Games"
+          onPress={() => this.state.navigate('Section', {sectionTitle: "Video Games Articles", section: "video-games", navigate: this.state.navigate})}/>
+        <Button title="Literature"
+          onPress={() => this.state.navigate('Section', {sectionTitle: "Literature Articles", section: "literature", navigate: this.state.navigate})}/>
       </View>
     );
   }
