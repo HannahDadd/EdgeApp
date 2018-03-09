e6c2e00ab7135b78c7cbd5f19e25cd59ed83e0e1
Hannah Dadd <hannahbdadd@googlemail.com>
2018-03-09 13:14:10 +0000

    Added recommended articles to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,96 @@ export default class HomeScreen extends React.Component {
     };
   }
 
+  // Get reccomeneded article for user
+  getRecommendedArticle(){
+    foundArticle = false;
+    const tags = await AsyncStorage.getItem("viewedTags");
+    const articlesRead = await AsyncStorage.getItem("viewedArticles"); 
+    while(!foundArticle){
+      // If they have not read any articles i.e. have no tags, suggest last article published      
+      if (tags !== null && tags.length !== 0){
+        // Go through all articles with first tag in list to find one to recommend
+        getTaggedArticles(tags[0]).map((article) => {
+          // If they've already read the article, don't recommend it
+          if(articlesRead.indexOf(article.id) > -1){
+            foundArticle = true;
+            return <View key={article.id}
+                      style={{flex: 1, flexDirection: 'column', padding: 10}}>
+                    <Text>Content recently Published</Text>
+                    <ArticleDisplay
+                      title={article.title.rendered}
+                      image={pic}
+                      onPressItem={() => navigate('ShowArticle', {article: article, image: pic})}
+                    />
+                  </View> 
+          }
+        });
+        // If article to recommend not found remove tag from list and try next tag in loop again
+        var index = tags.indexOf(tag[0]);
+        if (index > -1) {
+          tags.splice(index, 1);
+        }
+      } else {
+        // Return last article published
+        foundArticle = true;
+        return this.getLastArticlePublished();
+      }
+    }
+  }
+
+  // Get the tags JSON values
+  getTaggedArticles(tagID) {
+    fetch('https://www.theedgesusu.co.uk/wp-json/wp/v2/posts?tags=' + tagID + '&_embed', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(response => response.json())
+    .then(responseJson => {
+      return responseJson;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+
+  // Get last article published
+  getLastArticlePublished() {
+    fetch('https://www.theedgesusu.co.uk/wp-json/wp/v2/posts?per_page=1&_embed', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(response => response.json())
+    .then(responseJson => {
+      // Check if there is a featured image to display
+      article = responseJson[0];
+      let pic = '';
+      // Must use typeof as any part of 'pic[0].media_details.sizes.medium.source_url' can be undefined 
+      if(article && article._embedded && article._embedded['wp:featuredmedia'] !== undefined){
+        pic = article._embedded['wp:featuredmedia'];
+        if (pic[0].media_details && pic[0].media_details.sizes && pic[0].media_details.sizes.medium
+          && pic[0].media_details.sizes.medium.source_url !== undefined){
+          pic = pic[0].media_details.sizes.medium.source_url;
+        }
+      }
+      return <View key={article.id}
+                style={{flex: 1, flexDirection: 'column', padding: 10}}>
+              <Text>Content recently Published</Text>
+              <ArticleDisplay
+                title={article.title.rendered}
+                image={pic}
+                onPressItem={() => navigate('ShowArticle', {article: article, image: pic})}
+              />
+            </View>
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+
   componentDidMount () {
     // FCM.requestPermissions();
     // FCM.getFCMToken().then(token => {
@@ -32,7 +122,7 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{flex: 1, flexDirection: 'column', padding: 10}}>
-        <Text>Content coming soon :P</Text>
+        {this.getRecommendedArticle.bind(this)}
       </View>
     );
   }
--- screens/ShowArticleScreen.js
+++ screens/ShowArticleScreen.js
@@ -1,118 +1,167 @@
 import React from 'react';
-import { Text, View, Picker, Image, Button, ScrollView, WebView } from 'react-native';
+import { Text, View, Picker, Image, Button, ScrollView, AsyncStorage } from 'react-native';
 import AuthorDisplay from '../components/AuthorDisplay';
 import ArticleText from '../components/ArticleText';
 import EdgeSocialLinks from '../components/EdgeSocialLinks';
 import Styles from '../Styles';
 
 export default class ShowArticleScreen extends React.Component {
-    static navigationOptions = {
-      title: "Read Article",
+  static navigationOptions = {
+    title: "Read Article",
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.navigation.state.params.id,
+      title: this.props.navigation.state.params.article.title.rendered,
+      image: this.props.navigation.state.params.image,
+      content: this.props.navigation.state.params.article.content.rendered,
+      author: this.props.navigation.state.params.article.author,
+      authorName: '',
+      authorBio: '',
+      authorPic: '',
+      section: '',
+      tagIDs: this.props.navigation.state.params.article.tags,
+      tags: []
     };
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        id: this.props.navigation.state.params.id,
-        title: this.props.navigation.state.params.article.title.rendered,
-        image: this.props.navigation.state.params.image,
-        content: this.props.navigation.state.params.article.content.rendered,
-        author: this.props.navigation.state.params.article.author,
-        authorName: '',
-        authorBio: '',
-        authorPic: '',
-        section: '',
-        tagIDs: this.props.navigation.state.params.article.tags,
-        tags: []
-      };
-    }
+  // Fetch data from the api
+  getJSONData(searchIn, searchFor, callBack) {
+    fetch('https://www.theedgesusu.co.uk/wp-json/wp/v2/' + searchIn + '?search=' + searchFor + '&_embed', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(response => response.json())
+    .then(responseJson => {
+      console.log(responseJson);
+      callBack(responseJson);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
 
-    // Fetch data from the api
-    getJSONData(searchIn, searchFor, callBack) {
-      fetch('https://www.theedgesusu.co.uk/wp-json/wp/v2/' + searchIn + '?search=' + searchFor + '&_embed', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      }).then(response => response.json())
-      .then(responseJson => {
-        callBack(responseJson)
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    }
-
-    // Query rest api for data- use fetch api
-    componentDidMount() {
-      // Get author
-      this.getJSONData('users', this.state.author, 
+  // Query rest api for data- use fetch api
+  componentDidMount() {
+    // Get author
+    this.getJSONData('users', this.state.author, 
+      function(responseJson) {
+        if(responseJson && responseJson[0] !== undefined){
+          this.setState(
+            {authorName: responseJson[0].name,
+            authorBio: responseJson[0].description,
+            authorPic: responseJson[0]['avatar_urls'][96]
+          });
+        }
+      }.bind(this)
+    );
+    // Get tags from id
+    let tags = []
+    for (var i = 0; i <  this.state.tagIDs.length; i++) {
+      let key = this.state.tagIDs[i];
+      this.getJSONData('tags', this.state.tagIDs[i], 
         function(responseJson) {
           if(responseJson && responseJson[0] !== undefined){
-            this.setState(
-              {authorName: responseJson[0].name,
-              authorBio: responseJson[0].description,
-              authorPic: responseJson[0]['avatar_urls'][96]
-            });
+            tags.push({key: key, name: responseJson[0].name});
+            // Store tags of articles user is interested in for reccomendations
+            this.addTagIdToRecommender(key);
           }
         }.bind(this)
       );
-      // Get tags from id
-      let tags = []
-      for (var i = 0; i <  this.state.tagIDs.length; i++) {
-        let key = this.state.tagIDs[i];
-        this.getJSONData('tags', this.state.tagIDs[i], 
-          function(responseJson) {
-            if(responseJson && responseJson[0] !== undefined){
-              tags.push({key: key, name: responseJson[0].name});
-            }
-          }.bind(this)
-        );
-      }
-      this.setState({tags: tags})
     }
+    this.setState({tags: tags})
+  }
 
-    render() {
-      const { navigate } = this.props.navigation;
-      // Check if there is a featured image to display
-      var icon;
-      if(String(this.state.image) && this.state.image.includes('http')){
-        icon = {uri: this.state.image};
+  // Add read articles to datastore so they are not recommended articles they have already read
+  async addArticleIDtoReadArticles(articleID){
+    // Check if they are already following this tag, author or section
+    try {
+      const value = await AsyncStorage.getItem("viewedArticles");
+      if (value !== null){ 
+          // Add new tag author or section to db
+        AsyncStorage.mergeItem("viewedArticles", JSON.stringify(articleID), () => {
+            // Item is added to datastore
+        });
       } else {
-        icon = require('../pictures/noimage.jpg');
+          // Update what they are following
+        AsyncStorage.setItem("viewedArticles", articleID, () => {
+              // The item should now be added to the db
+        });
       }
-      // Remove html tags from content
-      var content = this.state.content.replace(/<(?:.|\n)*?>/gm, '');
-      // Add tags to the picker
-      let tags = this.state.tags.map((tag) => {
-        console.log("hello ", tag.key);
-        return <View key={tag.key} style={{flex: 1, flexDirection: 'column', padding: 10}}>
-                <Text onPress={() => navigate('BrowseArticles', 
-                  {name: tag.name, postsURL: tag["_links"]["wp:post_type"][0].href})}
-                  style={Styles.sheet.titleText}>{tag.name}
-                </Text>
-              </View>
-      })
-      return (
-        <View style={{flex: 1}}>
-          <ScrollView style={{flexDirection: 'column', padding: 10}}>
-              <Image source={icon} style={{width: 350, height: 200}}/>
-              <Text style={Styles.sheet.titleText}>{this.state.title}</Text>
-              <ArticleText/>
-              <Text>{content}</Text>
-              <Text>{this.state.section}</Text>
-              <AuthorDisplay
-                name={this.state.authorName}
-                id={this.state.author}
-                bio={this.state.authorBio}
-                profilePic={this.state.authorPic}
-              />
-              <Text>Article Tags:</Text>
-              {tags}
-              <EdgeSocialLinks/>
-          </ScrollView>
-          </View>
-      );
+    } catch (error) {
+      // Error retrieving data
+      console.log(error)
     }
   }
+
+  async addTagIdToRecommender(tagID){
+    try {
+      // If they are following the tag, don't recommend it
+      const tagsFollowing = await AsyncStorage.getItem("tags");
+      if (tagsFollowing === null || tagsFollowing.indexOf(tagID) < 0){
+        const value = await AsyncStorage.getItem("viewedTags");
+        if (value !== null){ 
+            // Add new tag author or section to db
+          AsyncStorage.mergeItem("viewedTags", JSON.stringify(tagID), () => {
+              // Item is added to datastore
+          });
+        } else {
+            // Update what they are following
+          AsyncStorage.setItem("viewedTags", tagID, () => {
+                // The item should now be added to the db
+          });
+        }
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.log(error)
+    }
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+    // Check if there is a featured image to display
+    var icon;
+    if(String(this.state.image) && this.state.image.includes('http')){
+      icon = {uri: this.state.image};
+    } else {
+      icon = require('../pictures/noimage.jpg');
+    }
+    // Remove html tags from content
+    var content = this.state.content.replace(/<(?:.|\n)*?>/gm, '');
+    // Add tags to the picker
+    let tags = this.state.tags.map((tag) => {
+      console.log("hello ", tag.key);
+      return <View key={tag.key} style={{flex: 1, flexDirection: 'column', padding: 10}}>
+              <Text onPress={() => navigate('BrowseArticles', 
+                {name: tag.name, postsURL: tag["_links"]["wp:post_type"][0].href})}
+                style={Styles.sheet.titleText}>{tag.name}
+              </Text>
+            </View>
+    })
+    return (
+      <View style={{flex: 1}}>
+        <ScrollView style={{flexDirection: 'column', padding: 10}}>
+            <Image source={icon} style={{width: 350, height: 200}}/>
+            <Text style={Styles.sheet.titleText}>{this.state.title}</Text>
+            <ArticleText/>
+            <Text>{content}</Text>
+            <Text>{this.state.section}</Text>
+            <AuthorDisplay
+              name={this.state.authorName}
+              id={this.state.author}
+              bio={this.state.authorBio}
+              profilePic={this.state.authorPic}
+            />
+            <Text>Article Tags:</Text>
+            {tags}
+            <EdgeSocialLinks/>
+        </ScrollView>
+        </View>
+    );
+  }
+}
