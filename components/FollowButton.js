3328bd582ce747ac013e8e3e2871b20a1f64b504
Hannah Dadd <hannahbdadd@googlemail.com>
2018-03-11 11:28:00 +0000

    Can follow items

--- components/FollowButton.js
+++ components/FollowButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Button, Text, AsyncStorage } from 'react-native';
 import Styles from '../Styles';
+import {FCMToken} from '../App.js';
 
 export default class FollowButton extends React.PureComponent {
     constructor(props) {
@@ -20,8 +21,10 @@ export default class FollowButton extends React.PureComponent {
            if (value !== null){ 
               // Add new tag author or section to db
               AsyncStorage.mergeItem("following", JSON.stringify(this.state.id), () => {
-                  // The item should now be added to the db
+                  // The item should now be added to the db, register it with push notifications
                   this.setState({followingItem: true});
+                  this.registerForNotifications();
+
             });
            }
            else {
@@ -29,12 +32,30 @@ export default class FollowButton extends React.PureComponent {
               AsyncStorage.setItem("following", this.state.id, () => {
                   // The item should now be added to the db
                   this.setState({followingItem: true});
+                  this.registerForNotifications();
               });
           }
         } catch (error) {
           // Error retrieving data
           console.log(error)
         }
+    }
+
+    // Follow on API
+    registerForNotifications(){
+        fetch('http://theedgesusu.co.uk/pnfw/categories/', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            token: FCMToken,
+            os: 'Android',
+            id: this.state.id,
+            exclude: false,
+        }),
+        });
     }
 
     // Button to follow an item
