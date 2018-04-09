f79918eec8dbb549ce3af8d9ff75e5236e924737
Hannah Dadd <hannahbdadd@googlemail.com>
2018-04-09 16:58:02 +0100

    Foreground notifications received and displayed

--- android/app/src/main/java/com/edgeapp/MyFirebaseMessagingService.java
+++ android/app/src/main/java/com/edgeapp/MyFirebaseMessagingService.java
@@ -1,5 +1,13 @@
 package com.edgeapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -8,4 +16,47 @@ import com.google.firebase.messaging.RemoteMessage;
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Show notification
+        try {
+            sendNotification(remoteMessage.getNotification().getBody(), remoteMessage.getMessageId());
+        } catch(Exception e){
+            // do nothing
+        }
+    }
+
+    private void sendNotification(String messageBody, String id) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, "my_channel_01")
+                        .setSmallIcon(R.drawable.edge)
+                        .setContentTitle("Article Published")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//
+//      /* Create or update. */
+//            NotificationChannel channel = new NotificationChannel("my_channel_01",
+//                    "Channel human readable title",
+//                    NotificationManager.IMPORTANCE_DEFAULT);
+//            mNotificationManager.createNotificationChannel(channel);
+//        }
+
+        notificationManager.notify(Integer.valueOf(id), notificationBuilder.build());
+    }
 }
